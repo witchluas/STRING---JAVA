1b962fb187bf0a4547f0c7e399dc8bae08fc885b
Luanna Evellin <145905994+witchluas@users.noreply.github.com>
2023-12-04 17:04:08 -0300

    Add files via upload

---
+++ exerciciostring03.java
@@ -0,0 +1,16 @@
+package arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class exerciciostring03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite um texto: ");
+		String str = entrada.nextLine();
+		String resultado = str.toUpperCase();
+		System.out.println(resultado);
+
+	}
+
+}
---
+++ exerciciotring02.java
@@ -0,0 +1,14 @@
+package arrays;
+
+public class exerciciotring02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String valor = "Será que são iguais";
+		System.out.println(valor.compareTo("Será que são iguais") == 0 ? true : false);
+		System.out.println(valor.compareTo("Será que são iguais")== 0? true : false);
+		
+		
+	}
+
+}
